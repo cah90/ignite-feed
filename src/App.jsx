f13f9c5333efffd7e8bc63a9ec4d99d8281436ca
Cássia <cascrisbern@gmail.com>
2023-10-04 15:36:33 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+import { Post } from "./Post"
+
+import styles from "./App.module.css"
+import "./global.css"
+
+function App() {
+	return (
+		<>
+			<Header />
+
+			<div className={styles.wrapper}>
+				<Sidebar />
+
+				<main>
+					<Post name="Ana Maria" content="Alguma comentário aqui" />
+					<Post name="Ana Paula" content="Alguma comentário aqui" />
+				</main>
+			</div>
+		</>
+	)
+}
+
+export default App
